5a51e78d0e194e7cc8e193463b5435c337ad4487
findaditya <findadityaagrawal@gmail.com>
2024-09-26 10:38:16 +0530

    added header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,15 @@
 import "./globals.css";
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Phooly Saas Landing Page",
+  description: "Created by Blaise labs",
+};
+
 
 export default function RootLayout({
   children,
@@ -6,9 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-     <body>
-     {children}
+    <html lang="en" className="relative">
+    <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+        {children}
       </body>
     </html>
   );
---
+++ src/sections/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+    return <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+        <p>Get Started for free</p>
+        </div>;
+};
